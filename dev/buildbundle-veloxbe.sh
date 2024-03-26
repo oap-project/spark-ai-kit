8ff487a549f107789d3f5009463e5d8a2df31a5b
Yuan Zhou <yuan.zhou@intel.com>
2024-03-26 10:54:13 +0800

    adding ai kit
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- dev/buildbundle-veloxbe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname $0)
-source "$BASEDIR/builddeps-veloxbe.sh"
-
-cd $GLUTEN_DIR
-mvn clean package -Pbackends-velox -Prss -Pspark-3.2 -DskipTests
-mvn clean package -Pbackends-velox -Prss -Pspark-3.3 -DskipTests
-mvn clean package -Pbackends-velox -Prss -Pspark-3.4 -DskipTests
--- dev/formatcppcode.sh
+++
@@ -1,3 +0,0 @@
-cd `dirname $0`
-find ../cpp/core -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-12 -style=file -i {} \;
-find ../cpp/velox -regex '.*\.\(cc\|hpp\|cu\|c\|h\)' -exec clang-format-12 -style=file -i {} \;
